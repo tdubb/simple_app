7761ee89167b82efba867fffd4ce87dbb9a5d1a8
Tyler Wasden <twasden@gmail.com>
2013-05-11 00:45:27 -0500

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,10 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def sign_in
+  end
+
+  def sign_up
+  end	
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,57 +5,57 @@ describe "StaticPages" do
   describe "Home page" do
   
     it "should have the h1 'Hi'" do
-    	visit '/static_pages/home'
-    	page.should have_selector('h1', :text => 'Hi')
+    	visit root_path
+    	page.should have_selector('h1', text: 'Hi')
       
     end
 
     it "should have the title 'Home'" do 
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',
-                          :text => "Bali | Home")
+                          text: "Bali | Home")
     end  
   end
 
   describe "Help page" do
 
   	it "should have the h1 'Do you need help'" do 
-  	  visit '/static_pages/help'
-  	  page.should have_selector('h1', :text => 'Do you need help?')
+  	  visit help_path
+  	  page.should have_selector('h1', text: 'Do you need help?')
   	end
 
     it "should have the right title 'Help'" do 
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
-                          :text => "Bali | Help" )
+                          text: "Bali | Help" )
     end 
   end	 
 
   describe "About page" do 
 
     it "should have the h1 'About my project'" do 
-      visit '/static_pages/about' 
-      page.should have_selector('h1', :text => 'About my project')
+      visit about_path 
+      page.should have_selector('h1', text: 'About my project')
     end
 
     it "should have the right title 'About'" do 
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
-                          :text => "Bali | About")
+                          text: "Bali | About")
     end  
   end  
 
   describe "Contact page" do 
 
     it "should have the h1 'Contact Me'" do 
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact Me')
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact Me')
     end
 
     it "should have the right title 'Contact Me'" do 
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title',
-                          :text => "Bali | Contact Me")
+                          text: "Bali | Contact Me")
     end 
   end    
 end
