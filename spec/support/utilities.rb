006c82901fe89ad2ee7ced78214b0571a6df2eb2
Tyler Wasden <twasden@gmail.com>
2013-05-18 21:57:09 -0500

    Finish user edit, update, index, and destroy actions

---
+++ spec/support/utilities.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+def sign_in(user)
+	visit sign_in_path
+	fill_in "Email",    with: user.email
+	fill_in "Password", with: user.password
+	click_button "Sign in"
+	# Sign in when not using Capybara as well.
+	cookies[:remember_token] = user.remember_token
+end	
